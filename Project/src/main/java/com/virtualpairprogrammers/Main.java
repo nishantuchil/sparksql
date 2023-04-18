e8ce7a6aa370e09ddb408276457afed5aa36db2c
nishantuchil <nishantuchil@gmail.com>
2023-04-18 19:59:56 +0530

    added code for in memory loading

--- Project/src/main/java/com/virtualpairprogrammers/Main.java
+++ Project/src/main/java/com/virtualpairprogrammers/Main.java
@@ -1,5 +1,8 @@
 package com.virtualpairprogrammers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LoggerContext;
@@ -7,7 +10,12 @@ import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
 public class Main {
 
@@ -30,18 +38,34 @@ public class Main {
             .config("spark.sql.warehouse.dir", "file:///c:/tmp/")
             .getOrCreate();
 
-    Dataset<Row> dataset =
-        spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
-
     // Dataset<Row> modernArtResults = dataset.filter("subject = 'Modern Art' AND year >= 2007");
     // modernArtResults.show();
 
+    List<Row> inMemory = new ArrayList<Row>();
+    inMemory.add(RowFactory.create("WARN", "2016-12-31 04:19:32"));
+    inMemory.add(RowFactory.create("FATAL", "2016-12-31 03:22:34"));
+    inMemory.add(RowFactory.create("WARN", "2016-12-31 03:21:21"));
+    inMemory.add(RowFactory.create("INFO", "2015-4-21 14:32:21"));
+    inMemory.add(RowFactory.create("FATAL", "2015-4-21 19:23:20"));
+
+    StructField[] fields =
+        new StructField[] {
+          new StructField("level", DataTypes.StringType, false, Metadata.empty()),
+          new StructField("datetime", DataTypes.StringType, false, Metadata.empty())
+        };
+    StructType schema = new StructType(fields);
+    Dataset<Row> dataset = spark.createDataFrame(inMemory, schema);
+    dataset.show();
+
+    /*
+    Dataset<Row> dataset =
+        spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
+
     dataset.createOrReplaceTempView("my_students_view");
 
-    Dataset<Row> results =
-        spark.sql("select max(score) from my_students_view where subject='French'");
+    Dataset<Row> results = spark.sql("select distinct(year) from my_students_view order by year");
     results.show();
-
+    */
     spark.close();
   }
 }
